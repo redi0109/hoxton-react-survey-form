209a97baa1dd73cdc05e93d4473c1cc2d6fc5d35
Demir Boraj <demirboraj97@gmail.com>
2022-08-05 12:16:28 +0200

    1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,9 +19,9 @@ const initialForm = {
   consistency: 0,
   colour: 0,
   logo: 0,
-  bestFeatures: [],
-  worstFeatures: [],
-  timeSpent: [],
+  bestFeatures: ["color"],
+  worstFeatures: ["logo"],
+  timeSpent: ["swimming"],
 };
 
 function Main() {
@@ -84,7 +84,7 @@ function Main() {
               <input type="checkbox" name="worstFeatures" value="logo" />
               It has a logo
             </label>
- 
+
             <label>
               <input type="checkbox" name="worstFeatures" value="size" />
               It's big
@@ -98,39 +98,114 @@ function Main() {
           <li>
             <label>
               <input type="radio" name="consistency" value="1" />
-             1
+              <span>1</span>
             </label>
 
             <label>
               <input type="radio" name="consistency" value="2" />
-             2
+              <span className="radio">2</span>
             </label>
 
             <label>
               <input type="radio" name="consistency" value="3" />
-              3
+              <span>3</span>
             </label>
- 
+
             <label>
               <input type="radio" name="consistency" value="4" />
-              4
+              <span>4</span>
             </label>
           </li>
         </div>
 
         <h3>How do you rate your rubber duck color</h3>
 
+        <div className="form__group radio">
+          <li>
+            <label>
+              <input type="radio" name="color" value="1" />
+              <span>1</span>
+            </label>
+
+            <label>
+              <input type="radio" name="color" value="2" />
+              <span className="radio">2</span>
+            </label>
+
+            <label>
+              <input type="radio" name="color" value="3" />
+              <span>3</span>
+            </label>
+
+            <label>
+              <input type="radio" name="color" value="4" />
+              <span>4</span>
+            </label>
+          </li>
+        </div>
+
         <h3>How do you rate your rubber duck logo</h3>
+
+        <div className="form__group radio">
+          <li>
+            <label>
+              <input type="radio" name="logo" value="1" />
+              <span>1</span>
+            </label>
+
+            <label>
+              <input type="radio" name="logo" value="2" />
+              <span className="radio">2</span>
+            </label>
+
+            <label>
+              <input type="radio" name="logo" value="3" />
+              <span>3</span>
+            </label>
+
+            <label>
+              <input type="radio" name="logo" value="4" />
+              <span>4</span>
+            </label>
+          </li>
+        </div>
 
         <h3>How do you like to spend the time with your rubber duck</h3>
 
+        <div className="form__group">
+          <li>
+            <label>
+              <input type="checkbox" name="timeSpent" value="swimming" />
+              Swimming
+            </label>
+
+            <label>
+              <input type="checkbox" name="timeSpent" value="bathing" />
+              Bathing
+            </label>
+
+            <label>
+              <input type="checkbox" name="timeSpent" value="chatting" />
+              Chatting
+            </label>
+
+            <label>
+              <input type="checkbox" name="timeSpent" value="noTime" />I don't
+              like to spent time with it
+            </label>
+          </li>
+        </div>
+
         <h3>What else have you got to say about your rubber duck?</h3>
+        <textarea name="review"></textarea>
 
         <h3>Put your name here (if you feel like it):</h3>
+        <input type="text" name="username" />
 
         <h3>Leave us your emaol pretty please??</h3>
+        <input type="email" name="email" />
 
-        <button className="form__ submit">Submit Survey!</button>
+        <button className="form__submit">Submit Survey!</button>
       </form>
     </main>
   );
